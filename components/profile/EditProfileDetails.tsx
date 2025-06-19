2c7228a132e1fbcc7d7a205b7a8661ab73cf3330
KewynMac <kirtsikhanau@gmail.com>
2025-06-19 11:23:18 +0300

    delete profile button

--- components/profile/EditProfileDetails.tsx
+++ components/profile/EditProfileDetails.tsx
@@ -1,12 +1,17 @@
-import {useProfileForm} from '@/hooks/useProfileForm'
-import {IProfile} from '@/types/profile'
 import PageWithHeader from '@/components/PageWithHeader'
 import ProfileDetails from '@/components/profile/ProfileDetails'
-import {updateItem} from '@/utils/AsyncStorage'
-import {USER} from '@/constants/localstorage'
-import {useState} from 'react'
 import Text from '@/components/ui/Text'
-import {Colors} from '@/constants/Colors'
+import { Colors } from '@/constants/Colors'
+import { IS_ONBOARDING_PASSED, USER } from '@/constants/localstorage'
+import { ROUTES } from '@/constants/routes'
+import { useProfileForm } from '@/hooks/useProfileForm'
+import { IProfile } from '@/types/profile'
+import { removeItem, updateItem } from '@/utils/AsyncStorage'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { router } from 'expo-router'
+import { useState } from 'react'
+import { View } from 'react-native'
+import ButtonCommon from '../ui/Buttons/ButtonCommon'
 
 interface IEditProfileDetailsProps {
     profile: IProfile;
@@ -30,6 +35,12 @@ const EditProfileDetails = (props: IEditProfileDetailsProps) => {
         handleCancelEdit()
     }
 
+    const deleteProfile = async () => {
+        await removeItem<IProfile>(USER, profile.id, refresh)
+        await AsyncStorage.removeItem(IS_ONBOARDING_PASSED)
+        router.replace(ROUTES.ONBOARDING)
+    }
+
     return (
       <PageWithHeader title={'navbar.profile'} leftBtnText={'button.back'} rightBtnText={'button.save'}
                       onPressLeftBtn={handleCancelEdit} onPressRightBtn={handleSaveProfile}>
@@ -37,6 +48,14 @@ const EditProfileDetails = (props: IEditProfileDetailsProps) => {
           {error && <Text text={error} textColor={Colors.light.error} styles={{
               margin: 'auto'
           }}/>}
+
+          <View style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              gap: 6
+          }}>
+              <ButtonCommon title={'button.delete'} bgColor={Colors.light.error} onPress={() => deleteProfile()}/>
+          </View>
       </PageWithHeader>
     )
 }
